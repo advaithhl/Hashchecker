b4b0cfd12c90cef12916185cdbb6493a2be09364
Advaith HL <niceadvaith@gmail.com>
2018-08-09 21:29:36 +0530

    Added verify and pretty printing

--- core/FileObject.py
+++ core/FileObject.py
@@ -49,3 +49,27 @@ class FileObject:
                 buffer = file_handle.read(self.__BLOCKSIZE)
 
         return hasher.hexdigest()
+
+    def verify(self, checksum):
+        if len(checksum) == 32:
+            return self.md5() == checksum
+        elif len(checksum) == 40:
+            return self.sha1() == checksum
+        elif len(checksum) == 64:
+            return self.sha256() == checksum
+        elif len(checksum) == 128:
+            return self.sha512() == checksum
+        else:
+            return False
+
+    def __eq__(self, other):
+        return self.md5() == other.md5()
+
+    def __str__(self):
+        md5 = self.md5()
+        sha1 = self.sha1()
+        sha256 = self.sha256()
+        sha512 = self.sha512()
+        return path.basename(
+            self.filepath)+'\n\n'+'MD5    : '+md5 + \
+            '\nSHA1   : '+sha1 + '\nSHA256 : '+sha256 + '\nSHA512 : '+sha512
