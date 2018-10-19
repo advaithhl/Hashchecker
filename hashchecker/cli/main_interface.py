bd401363400acded2e33b3f879556986f9a1bd92
Advaith HL <niceadvaith@gmail.com>
2018-10-19 23:42:07 +0530

    Rudimentary CLI (2/3)

--- hashchecker/cli/main_interface.py
+++ hashchecker/cli/main_interface.py
@@ -1,6 +1,7 @@
 from hashchecker.core.coreactions.Calculate import Calculate
 from hashchecker.core.coreactions.Duplicate import Duplicate
 from hashchecker.core.coreactions.Verify import Verify
+from hashchecker.parsing.ArgParse import parse_args
 from hashchecker.parsing.FileObjectParser import FileObjectParser
 from hashchecker.cli.color_string import *
 
@@ -28,3 +29,26 @@ def verify_cli(args):
 
     for (file_name, result) in results.items():
         print(pretty_result(file_name, result))
+
+
+def calculate_cli(args):
+    hashtypes = []
+    for hashtype in parse_args().hashtypes:
+        if hashtype == 'md5':
+            hashtypes.append(Calculate.md5)
+        elif hashtype == 'sha1':
+            hashtypes.append(Calculate.sha1)
+        elif hashtype == 'sha256':
+            hashtypes.append(Calculate.sha256)
+        elif hashtype == 'sha512':
+            hashtypes.append(Calculate.sha512)
+
+    c = Calculate()
+    for result in c.calculate(hashtypes):
+        print('file: {}\n+ md5: {}\n+ sha1: {}\n+ sha256: {}\n+ sha512: {}'.format(
+            blue(result.file_object.get_name()),
+            result.md5,
+            result.sha1,
+            result.sha256,
+            result.sha512
+        ))
